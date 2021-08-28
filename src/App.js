9ea09fd635078f5f6fa3b62b5badc44200030be2
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-08-29 02:26:23 +0530

    initial commit, adds basic functionality to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+const db = {
+  "😬": "Grimacing Face",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😂": "Face with Tears of Joy",
+  "😍" :"Smiling Face with Heart-Eyes",
+  "😎" : "Smiling Face with Sunglasses",
+  "😑" : "Expressionless Face",
+  "🤐" : "Zipper-Mouth Face",
+  "🙄" : "Face with Rolling Eyes",
+  "😛" : "Face with Tongue"
+}
+
+const emojis = Object.keys(db);
 
 function App() {
+
+  const [inputValue, setInputValue] = useState("");
+  const [meaning,setMeaning] = useState("");
+  const inputChangeHandler = (event) => {
+      setInputValue(event.target.value.slice(0,2));
+  }
+
+  useEffect(() => {
+    inputValue.length === 0 ? setMeaning("Waiting for your input") : setMeaning(db[inputValue])
+  },[inputValue])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className="header">
+      <div className="header-upper">
+        <h1 className="heading">Emoji Interpreter</h1>
+      </div>
+      <div className="header-lower">
+        <h5 className="sub-heading">Learn the meaning of emojis you love</h5>
+      </div>
+    </header>
+    <main className="main">
+      <input type="text" className="input" placeholder="Enter your emoji here..."  value={inputValue} onChange={inputChangeHandler}/>
+      <small>Emoji Meaning</small>
+      <h1>{meaning}</h1>
+      <div className="emoji-container">
+      {
+        emojis.map((item) => {
+          return (
+            <h1 className="emoji" key={item}
+            onClick={() => setMeaning(db[item])}
+            >{item}</h1>
+          )
+        })
+      }
+      </div>
+    </main>
+     <footer className="footer">
+      <div className="footer-columns">
+        <h4 className="footer-heading">Emoji Interpreter</h4>
+        <small className="small">Made with love and Reactjs</small>
+        <a className="footer-link" href="/">Source code</a>
+        <a className="footer-link" href="/">Learn to make your own</a>
+      </div>
+      <div className="footer-columns">
+        <h4 className="footer-heading">Made by</h4>
+        <small className="small"
+          >Mobashir Farhan. Feel free to connect and share your feedback</small
         >
-          Learn React
-        </a>
-      </header>
+        <a className="footer-link" href="/">Github</a>
+        <a className="footer-link" href="/">Twitter</a>
+        <a className="footer-link" href="/">LinkedIn</a>
+      </div>
+      
+    </footer>
     </div>
   );
 }
